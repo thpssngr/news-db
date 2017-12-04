6c1e88f9d3431ff6e058d18f5c22c782f827f25d
thpssngr <thpssngr@gmail.com>
2017-12-04 16:35:11 +0100

    changed order of db connection calls to first generate the views.

--- newsqueries.py
+++ newsqueries.py
@@ -6,13 +6,14 @@ import os
 # nanodegree backend project, and to get back results for different questions
 # about the data included.
 
-# connect to the new database via the postgresql module
-c = psycopg2.connect("dbname=news")
-cursor = c.cursor()
+# create views to prepare the data for each analysis.
 os.system('psql -d news -a -f page_views.sql')
 os.system('psql -d news -a -f author_views.sql')
 os.system('psql -d news -a -f errorlogs.sql')
 
+# connect to the new database via the postgresql module
+c = psycopg2.connect("dbname=news")
+cursor = c.cursor()
 
 # 1. What are the most popular three articles of all time?
 
@@ -58,7 +59,7 @@ cursor.execute(
 percent = cursor.fetchall()
 
 # format the result as an easy to read list
-print "\n1. ON WHICH DAYS DID MORE THAN 1 PERCENT OF REQUESTS LEAD TO ERRORS?"
+print "\n1. ON WHICH DAYS DID MORE THAN 1% OF REQUESTS LEAD TO ERRORS?\n"
 for day, number in zip(dates, percent):
     day = str(day[0])
     number = str(number[0])
